68541112c7a2d6546d8c4dc1e92bb53f1c956421
Chris Natke <cgnatke@gmail.com>
2017-04-10 18:29:57 -0700

    First commit of these files. Txt files are used as input (except compile instructions) and java files are two classes which contain inner mapper and reducer classes, along with code to run the map-reduce job.

---
+++ killCDHServices.sh
@@ -0,0 +1,17 @@
+sudo service hue stop
+sudo service impala-server stop
+sudo service impala-catalog stop
+sudo service impala-state-store stop
+sudo service oozie stop
+sudo service hive-server2 stop
+sudo service hive-webhcat-server stop
+sudo service hive-metastore stop
+sudo service flume-ng-agent stop
+sudo service sqoop-metastore stop
+sudo service sqoop2-server stop
+
+
+
+#sudo service hadoop-mapreduce-historyserver stop
+#sudo service hadoop-mapreduce-historyserver stop
+#sudo service hadoop-yarn-nodemanager stop
